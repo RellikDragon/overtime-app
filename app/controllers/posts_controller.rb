6ce55b0ffc44f151b6982990f23ee566193ec3ff
Jared Dunn <rellikdragon@yahoo.com>
2018-02-20 13:48:07 -0700

    Implemented user connection to posts with tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:date, :rationale))
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
 
     if @post.save
       redirect_to @post, notice: 'Your post was created successfully'
