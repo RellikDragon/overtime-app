c3d92925ef5d3e3e891910e7c7a0bade35385283
Jared Dunn <rellikdragon@yahoo.com>
2018-02-28 16:14:59 -0700

    Implemented tests and page redirection for permission structure.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -5,12 +5,12 @@ describe 'navigate' do
     @user = FactoryBot.create(:user)
     login_as(@user, :scope => :user)
   end
-  
+
   describe 'index' do
     before do
       visit posts_path
     end
-    
+
     it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
@@ -28,7 +28,7 @@ describe 'navigate' do
   end
 
   describe 'new' do
-    it "has a link from the homepage" do
+    it 'has a link from the homepage' do
       visit root_path
 
       click_link("new_post_from_nav")
@@ -50,7 +50,7 @@ describe 'navigate' do
     before do
       visit new_post_path
     end
-    
+
     it 'has a new form that can be reached' do
       expect(page.status_code).to eq(200)
     end
@@ -63,7 +63,7 @@ describe 'navigate' do
       expect(page).to have_content("Some rationale")
     end
 
-    it 'will have a user associated with it' do
+    it 'will have a user associated it' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "User Association"
       click_on "Save"
@@ -74,24 +74,29 @@ describe 'navigate' do
 
   describe 'edit' do
     before do
-      @post = FactoryBot.create(:post)
-    end
-
-    it 'can be reached by clicking edit on index page' do
-      visit posts_path
-
-      click_link("edit_#{@post.id}")
-      expect(page.status_code). to eq(200)
+      @edit_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      login_as(@edit_user, :scope => :user)
+      @edit_post = Post.create(date: Date.today, rationale: "asdf", user_id: @edit_user.id)
     end
 
     it 'can be edited' do
-      visit edit_post_path(@post)
+      visit edit_post_path(@edit_post)
 
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Edited content"
       click_on "Save"
 
       expect(page).to have_content("Edited content")
+    end
+
+    it 'cannot be edited by a non authorized user' do
+      logout(:user)
+      non_authorized_user = FactoryBot.create(:non_authorized_user)
+      login_as(non_authorized_user, :scope => :user)
+
+      visit edit_post_path(@edit_post)
+
+      expect(current_path).to eq(root_path)
     end
   end
 end
