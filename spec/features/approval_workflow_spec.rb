6281b262a1d7cdc12a01f797efec4c76af105f23
Jared Dunn <rellikdragon@yahoo.com>
2018-03-02 13:50:31 -0700

    Implemented ability to block users after post marked complete.

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+
 describe 'navigate' do
   before do
     @admin_user = FactoryBot.create(:admin_user)
@@ -18,14 +19,26 @@ end
       expect(@post.reload.status).to eq('approved')
     end
 
-    it 'cannot be edited by an admin' do
+    it 'cannot be edited by a non admin' do
       logout(:user)
-      @user = FactoryBot.create(:user)
-      login_as(@user, :scope => :user)
+      user = FactoryBot.create(:user)
+      login_as(user, :scope => :user)
 
       visit edit_post_path(@post)
 
       expect(page).to_not have_content('Approved')
+    end
+
+    it 'should not be editable by the post creator if status is approved' do
+      logout(:user)
+      user = FactoryBot.create(:user)
+      login_as(user, :scope => :user)
+
+      @post.update(user_id: user.id, status: 'approved')
+
+      visit edit_post_path(@post)
+
+      expect(current_path).to eq(root_path)
     end
   end
 end
