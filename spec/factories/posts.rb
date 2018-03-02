2729fc918b1a4a736f17afaf2d40dec9f680b940
Jared Dunn <rellikdragon@yahoo.com>
2018-03-02 15:13:48 -0700

    Added hours requested to show model

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,18 +2,14 @@ FactoryBot.define do
   factory :post do
     date Date.today
     rationale "Some Rationale"
+    overtime_request 3.5
     user
   end  
 
   factory :second_post, class: "Post" do
     date Date.yesterday
     rationale "Some More Content"
+    overtime_request 0.5
     user
-  end
-
-  factory :post_from_other_user, class: "Post" do
-    date Date.yesterday
-    rationale "Some More Content"
-    non_authorized_user
   end
 end
